9cb02807b4ac286b52aa598688ef947335488ead
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-07-28 01:36:09 -0500

    Se agrega plugin plumber y se configura archivo gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
-const { src, dest } = require("gulp");
+const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
-function css() {
-  return src("src/scss/app.scss") // Identificar archivo CSS
-    .pipe(sass()) // Compilarlo
+function css(done) {
+  src("src/scss/**/*.scss") // Identificar archivo SCSS a compilar
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "compressed" })) // Compilarlo
     .pipe(dest("build/css/app.css")); // Almacenarlo en el disco duro
+
+  done();
 }
-
-const { watch } = require("gulp");
-
-function dev() {
+function dev(done) {
   watch("src/scss/**/*.scss", css); // Observar cambios en todos los archivos .scss
+
+  done();
 }
 
 exports.css = css;
