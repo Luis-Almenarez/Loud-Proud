822cb6e88a52fe36fe6ffc7e856d552b86ed5199
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-07-31 18:28:27 -0500

    Se agregan estilos y funcionalidad a la sección de galeria, se agrega también versión mobile

--- src/JS/app.js
+++ src/JS/app.js
@@ -20,6 +20,46 @@ function createGalery() {
 
       `;
 
+    image.onclick = function () {
+      showImage(i);
+    };
     galery.appendChild(image);
   }
+}
+
+function showImage(id) {
+  const image = document.createElement("picture");
+  image.innerHTML = `
+    
+    <picture>
+    <source srcset="./build/img/grande/${id}.webp" type="image/webp" />
+    <img loading="lazy" width="200" height="300" src="./build/img/grande/${id}.jpg" alt="Img galery" />
+  </picture>
+
+    `;
+  // CREA OVERLAY CON IMAGEN
+  const overlay = document.createElement("div");
+  overlay.appendChild(image);
+  overlay.classList.add("overlay");
+  overlay.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fixed-body");
+    overlay.remove();
+  };
+
+  //BOTÓN PARA CERRAR EL MODAL
+  const closeModal = document.createElement("p");
+  closeModal.textContent = "X";
+  closeModal.classList.add("btn-close");
+  closeModal.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fixed-body");
+    overlay.remove();
+  };
+  overlay.appendChild(closeModal);
+
+  //AÑADE AL  HTML
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.classList.add("fixed-body");
 }
